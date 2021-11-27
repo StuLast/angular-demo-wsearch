27361555bc23f6e7db1653863bb513158fbfd3a5
Stu Last <stuart@last-family.co.uk>
2021-11-27 17:26:26 +0000

    Implements app scaffold and search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  onSearchTerm = (term: string):void => {
+    console.log(term);
+  }
 }
